ce979cf3835285c75b5ac10361f91cbcdf55eea6
oliur <oliur1549@gmail.com>
2020-06-06 22:51:25 +0600

    3 program commit

---
+++ Exam2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name, Age, City;
+            Console.Write("Enter Name :");
+            Name =Console.ReadLine();
+
+            Console.Write("Enter Age :");
+            Age = Console.ReadLine();
+
+            Console.Write("Enter City :");
+            City = Console.ReadLine();
+
+            List<string> names = new List<string>() { Name, Age, City };
+            
+
+            var redult = from element in names
+                          orderby Name
+                          select element;
+
+            foreach (var item in redult)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.Write("Enter first A/P (A for 12 hours P for 24 hours format) :");
+            a = Console.ReadLine();
+
+            string time;
+            
+            time = Console.ReadLine();
+            if (a == "A")
+            {
+                Console.Write("Enter your Time {00:00:00} FORMAT:");
+                converttime12(time);
+            }
+            else if (a == "P")
+            {
+                Console.Write("Enter your Time {00:00:00 PM} FORMAT:");
+                CONVERTTIME24(time);
+            }
+        }
+        static void CONVERTTIME24(String str)
+        {
+
+            int h1 = (int)str[1] - '0';
+            int h2 = (int)str[0] - '0';
+            int hh = (h2 * 10 + h1 % 10);
+
+          
+            if (str[8] == 'A')
+            {
+                if (hh == 12)
+                {
+                    Console.Write("00");
+                    for (int i = 2; i <= 7; i++)
+                        Console.Write(str[i]);
+                }
+                else
+                {
+                    for (int i = 0; i <= 7; i++)
+                        Console.Write(str[i]);
+                }
+            }
+
+           
+            else
+            {
+                if (hh == 12)
+                {
+                    Console.Write("12");
+                    for (int i = 2; i <= 7; i++)
+                        Console.Write(str[i]);
+                }
+                else
+                {
+                    hh = hh + 12;
+                    Console.Write(hh);
+                    for (int i = 2; i <= 7; i++)
+                        Console.Write(str[i]);
+                }
+            }
+        }
+        static void converttime12(string str)
+        {
+            int h1 = (int)str[0] - '0';
+            int h2 = (int)str[1] - '0';
+
+            int hh = h1 * 10 + h2;
+
+            string times;
+            if (hh < 12)
+            {
+                times = "AM";
+            }
+            else
+                times = "PM";
+
+            hh %= 12;
+
+            if (hh == 0)
+            {
+                Console.Write("12");
+
+                for (int i = 2; i < 8; ++i)
+                {
+                    Console.Write(str[i]);
+                }
+            }
+            else
+            {
+                Console.Write(hh);
+                for (int i = 2; i < 8; ++i)
+                {
+                    Console.Write(str[i]);
+                }
+            }
+            Console.WriteLine(" " + times);
+        }
+    }
+}
